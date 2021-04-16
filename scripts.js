dab32baa73d5efb52268612a0008e9cdd8a39a1d
i-mchedlishvili <imchedlishvili32@gmail.com>
2021-04-16 16:07:49 +0400

    added some features

--- scripts.js
+++ scripts.js
@@ -1,49 +1,84 @@
-let averageGrade = 6;
 let subjects = ["kotlin", "js", "python"];
 let students = [
 {
     name: "john",
     lastname: "smith",
-    grades: [["10", "7", "3", "9"], ["10", "7", "3", "9"], ["10", "7", "3", "9"]],
+    grades: ["8", "7", "3"],
 },
 {
     name: "george",
     lastname: "wesson",
-    grades: [["10", "7", "3", "9"], ["10", "7", "3", "9"], ["10", "7", "3", "9"]],
+    grades: ["5", "7", "3"],
 },
 {
     name: "harold",
     lastname: "wilson",
-    grades: [["10", "7", "3", "9"], ["10", "7", "3", "9"], ["10", "7", "3", "9"]],
+    grades: ["6", "7", "3"],
 }
 ];
 
 
-let subj = document.getElementById("subjectsList");
-function getSubjects() {
-  for(let i = 0; i < subjects.length; i++) {
-    let templateSubjects = `
-    <th>        
-   ${subjects[i]}
-    </th>
-`
+let getNames = document.getElementById("names");
+let table = document.getElementById("table");
+let getSubjects = document.getElementById("subjects");
 
-    subj.innerHTML += templateSubjects;
+document.getElementById("add-subject").addEventListener('click', function addSubject() {
+  let getSubjectValue = document.getElementById("subject-input").value;
+  if(getSubjectValue == "") {
+    alert("PLEASE FILL INPUT")
+    return false;
+  } else {
+    getSubjects.innerHTML += `<th>${getSubjectValue}</th>`
+    
   }
-}
-getSubjects();
+})
 
-let fullName = document.getElementById("name");
+document.getElementById("add-but").addEventListener('click', function add() {
+  let getStudentValue = document.getElementById("add-student").value;
+  let getGradeValue = document.getElementById("add-grades").value;
+  if (getGradeValue == "", getStudentValue == "", getStudentValue && getGradeValue == "" || getGradeValue && getStudentValue == "") {
+    alert("PLEASE FILL ALL INPUTS")
+    return false;
+  } else {
+  getSubjects.innerHTML += `<td>${getGradeValue}</td>`
+  getNames.innerHTML += `<td>${getStudentValue}</td>`
+  }
+})
 
-function getName() {
-    for(let i = 0; i < students.length; i++) {
-        let fName = `<td>
-        ${students[i].name += students[i].lastname}
-                        </td>`
-        fullName.innerHTML += fName;
-    }
-}
-getName();
+for (var k = 0; k < subjects.length; k++) {
+    //let subs = subjects[k];
+     //console.log(subs)
+   getSubjects.innerHTML += `<th>${subjects[k]}</th>`
+   
+  }
+
+for(var i = 0; i < students.length; i++) {
+    getNames.innerHTML += `
+    <td>${students[i].name + " " + students[i].lastname}</td>`
+     getSubjects.innerHTML += `<td>${students[i].grades}</td>`
+       
+   }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
