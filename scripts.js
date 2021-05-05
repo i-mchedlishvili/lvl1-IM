425c2215077b79544b52878e1c753a9ba0bfaece
i-mchedlishvili <imchedlishvili32@gmail.com>
2021-05-05 22:58:56 +0400

    working-result

--- scripts.js
+++ scripts.js
@@ -1,104 +1,29 @@
-
 const container = document.querySelector('.search-cont');
 const searchInput = document.querySelector('#search-input');
 const musicList = document.querySelector('#musicList');
 
-//let ragaca = [];
-function getContent() {
+function getContent() { //get results in HTML
   apiURL = 'https://itunes.apple.com/search?term=';
   apiURL += searchInput.value;
-  fetch( apiURL, {method: 'GET'})
+  fetch( apiURL, {method: 'POST'})
   .then(Response => Response.json())
-
-  .then(data => console.log(data.results));
-  
-  
+  .then(data => { 
+      let content = data.results;
+      let template = '';
+      for(let i = 0; i < content.length; i++) {
+        template += `<li class="artist-name">${content[i].artistName}</li>
+                      <li class="music-name">${content[i].trackName}</li>
+                      <li class="album-name">${content[i].collectionName}</li>
+                     <a href="${content[i].collectionViewUrl}" target="_blank"> <li class="linki">Link</li> </a>        
+                    `;
+      }
+      musicList.innerHTML += template;
+  })
   };
   
-
-//data => console.log(data)
+//enter button event for show results
 searchInput.addEventListener('keydown', event => {
   if (event.code === 'Enter') {
     return getContent();
   };
-})
-/*
-const getContent = (search) => {
-  const url = new URL('https://itunes.apple.com/search');
-  const params = { term: search, media: 'music'}
-  url.search = new URLSearchParams(params);
-  fetch(url, { method: 'POST'} )
-    .then(results => results.json())
-*/
-
-
-
-/*
-let searchBar = document.getElementById('search-input');
-let musicList = document.getElementById('musicList');
-let music = [];
-
-
-
-
-
-searchBar.addEventListener('keydown', (e) => {
-    const searchString = e.target.value;
-    music.filter(character => {
-        character.name.contains(searchString)
-    })
 });
-
-const loadMusic = async () => {
-    try {
-        let res = await fetch('https://itunes.apple.com/search');
-        let music = await res.json();
-        displayMusic(music);
-        console.log(music);
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-
-
-let displayMusic = (musics) => {
-    let appendHtml = musics 
-    .map((musics) => {
-        return `<li class="music">
-        <p>${musicname}</p>
-        
-        </li>`;
-    })
-};
-
-
-*/
-
-
-
-
-   /* .then(data => {
-      results = data.results;
-      const resultsHTML = results.map(
-        (result, index) => `
-          <div style="background-image: url(${result.artworkUrl100});" onclick="openMedia('${result.previewUrl}', '${result.trackCensoredName}')" class="result"></div>
-        `)
-         .join('');
-        container.innerHTML = resultsHTML;
-        searchElem.blur();
-        return fetch(data.results[0].artistViewUrl)
-      })
-      .then(data => data.text())
-      .then(data => {
-        const artistImgUrl = data.match(/https?:\/\/[a-zA-Z0-9:\/\.\-]+.jpg/)[0];
-        artistImage.style['background-image'] = `url(${artistImgUrl})`;
-      })
-      .catch(() => container.innerHTML = '<h1>Problem retrieving media...</h1>');
- }
-
-
-searchElem.addEventListener('keydown', getContent);
-
-
-*/
