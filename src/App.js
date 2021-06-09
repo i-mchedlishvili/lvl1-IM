3fd2fa8396b73c9ff63186ad732c945c4f05d149
i-mchedlishvili <imchedlishvili32@gmail.com>
2021-06-09 19:02:40 +0400

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import './resetCSS.css';
+/*import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)*/
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Todos</p>
+     <div className="submit-input">
+       <input className="input"></input>
+       <button>Submit</button>
+     </div>
+     <div className="items">
+       <ul>
+         <li>sdfsdf</li>
+         <li>sdfsdf</li>
+         <li>sdfsdf</li>
+         <li>sdffddf</li>
+       </ul>
+     </div>
     </div>
   );
 }
